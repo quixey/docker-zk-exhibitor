7d6f44a7878eb3a050d1c1b83fd664aa51ce9c62
Chris Hoffman <choffman@quixeyy.com>
2016-09-08 13:47:40 -0700

    Quixey-ize for kirk deploy

--- run_image.sh
+++ run_image.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 
-DEFAULT_QENV='dev'
-DEFAULT_LOG_DIR='/var/log/docker/zookeeper'
-: ${QENV:=$DEFAULT_QENV}
-: ${LOG_DIR:=$DEFAULT_LOG_DIR}
+source docker/config_bash
 
 docker run -d -p 8181:8181 -p 2181:2181 -p 2888:2888 -p 3888:3888 \
     -e S3_BUCKET="quixey-${QENV}" \

8de30daa417fb552a57b5896164865160961bb88
Chris Hoffman <choffman@quixeyy.com>
2016-11-04 14:12:01 -0700

    adding additional connection-saturation tool for test_cluster

--- test/test_cluster.py
+++ test/test_cluster.py
@@ -1,14 +1,22 @@
 #!/usr/bin/env python
 import argparse
+import logging
+import string
 import time
 import random
-import string
 
+from collections import Counter
+
+import gevent
 from kazoo import client
-from kazoo.exceptions import NoNodeError
+from kazoo.exceptions import NoNodeError, SessionExpiredError
+from kazoo.handlers.threading import KazooTimeoutError
 
 WAIT_TIME = .005
 ID_SIZE = 4
+MAX_POOL_SIZE = 340
+
+logging.basicConfig()
 
 
 def clear_foos(zk):
@@ -49,15 +57,57 @@ def pound_host(hostname):
         clear_foos(zk)
 
 
+def make_connection(hostname, i):
 
+    logging.info('creating connection ', i)
+    zk = client.KazooClient(hostname)
+    try:
+        zk.start(timeout=.3)
+    except KazooTimeoutError:
+        logging.debug('connection {}, TIMEOUT'.format(i))
+        return "Timeout"
+    return "OK"
+
+
+def test_number_of_connections(hostname, pool_size=MAX_POOL_SIZE, hold=True):
+    threads = []
+
+    for i in xrange(pool_size):
+        threads.append(gevent.spawn(make_connection, hostname, i))
+
+    gevent.joinall(threads)
+
+    c = Counter()
+    for t in threads:
+        print t.value
+        c.update([t.value])
+    print c
+
+    if hold:
+        raw_input('Holding connections open. Press any key to close connections end...')
+
+    return c
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-o', '--host', default='zookeeper.stage.w1.us.quixey.com')
+    parser.add_argument('-t', '--test-connections', action='store_true')
+    parser.add_argument('-c', '--connections', default=MAX_POOL_SIZE, type=int)
+    parser.add_argument('-n', '--no-hold', action='store_false', default=True)
     args = parser.parse_args()
 
-    pound_host(args.host)
+    if args.test_connections:
+        if args.connections > MAX_POOL_SIZE:
+            print "Max Connections: {}. Setting to Max Connections, now.".format(MAX_POOL_SIZE)
+            args.connections = MAX_POOL_SIZE
+        test_number_of_connections(args.host, args.connections, args.no_hold)
+    else:
+        while True:
+            try:
+                pound_host(args.host)
+            except (SessionExpiredError, KazooTimeoutError) as e:
+                print 'client terminated: restarting now'
 
 
 if __name__ == "__main__":
